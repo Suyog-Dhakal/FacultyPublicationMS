f192d579145f3baee4730f056717dd166d70057d
Suyog-Dhakal <suyogdhakal8@gmail.com>
2023-01-14 15:17:07 +0545

    profile pic added and paper scraped

--- source/frontend/src/components/papers/Scraping_test.js
+++ source/frontend/src/components/papers/Scraping_test.js
@@ -9,7 +9,7 @@ async function getCitation() {
   let driver = new Builder().forBrowser("chrome").build();
 
   await driver.get(
-    "https://scholar.google.com/citations?user=O3HvZ_IAAAAJ&hl=en"
+    "https://scholar.google.com/citations?user=rIe2LUAAAAAJ&hl=en"
   );
 
   await driver.findElement(By.css("button#gsc_bpf_more")).click();
