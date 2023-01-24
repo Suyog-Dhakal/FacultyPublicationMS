c75e296fbee1c1a4615ceae44b912173aa8c80c1
Suyog-Dhakal <suyogdhakal8@gmail.com>
2023-01-24 14:13:50 +0545

    hemrajpant and Triratna 100 papers added

--- source/frontend/src/components/papers/Scraping_test.js
+++ source/frontend/src/components/papers/Scraping_test.js
@@ -9,9 +9,10 @@ async function getCitation() {
   let driver = new Builder().forBrowser("chrome").build();
 
   await driver.get(
-    "https://scholar.google.com/citations?user=XN-sooAAAAAJ&hl=en&oi=ao"
+    "https://scholar.google.com/citations?user=0tHzAygAAAAJ&hl=en"
   );
 
+  await driver.findElement(By.css("button#gsc_bpf_more")).click();
   await driver.findElement(By.css("button#gsc_bpf_more")).click();
   await driver.wait(until.elementLocated(By.css("span#gsc_a_nn")), 5000);
   const iframe = await driver.findElement(By.css("span#gsc_a_nn"));
