25439b75d258a2d7eb03c0c7d021e9ca37eb3757
Suyog-Dhakal <suyogdhakal8@gmail.com>
2023-02-05 12:36:50 +0545

    bar graph

--- source/frontend/src/components/PaperAnalytics.jsx
+++ source/frontend/src/components/PaperAnalytics.jsx
@@ -1,10 +1,27 @@
 import { NetworkPing } from "@mui/icons-material";
 import { MenuItem, Select } from "@mui/material";
 import React, { useEffect } from "react";
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+} from "recharts";
 
 const PaperAnalytics = () => {
   const [papers, setPapers] = React.useState([]);
-  const [username, setUserName] = React.useState([]);
+
+  const departmentAnalytics = {
+    Civil: 0,
+    Architecture: 0,
+    "Applied Science and Chemical": 0,
+    "Electronics and Computer": 0,
+    Mechanical: 0,
+    Electrical: 0,
+  };
 
   useEffect(() => {
     fetch("http://127.0.0.1:8000/api/papers/")
@@ -14,6 +31,74 @@ const PaperAnalytics = () => {
         setPapers(papers);
       });
   }, []);
+
+  departmentAnalytics.Civil = papers.reduce(
+    (count, paper) =>
+      paper.author.profile.department === "Civil" ? count + 1 : count,
+    0
+  );
+  departmentAnalytics.Architecture = papers.reduce(
+    (count, paper) =>
+      paper.author.profile.department === "Architecture" ? count + 1 : count,
+    0
+  );
+  departmentAnalytics["Applied Science and Chemical"] = papers.reduce(
+    (count, paper) =>
+      paper.author.profile.department === "Applied Science and Chemical"
+        ? count + 1
+        : count,
+    0
+  );
+  departmentAnalytics["Electronics and Computer"] = papers.reduce(
+    (count, paper) =>
+      paper.author.profile.department === "Electronics and Computer"
+        ? count + 1
+        : count,
+    0
+  );
+  departmentAnalytics.Mechanical = papers.reduce(
+    (count, paper) =>
+      paper.author.profile.department === "Mechanical" ? count + 1 : count,
+    0
+  );
+  departmentAnalytics.Electrical = papers.reduce(
+    (count, paper) =>
+      paper.author.profile.department === "Electrical" ? count + 1 : count,
+    0
+  );
+
+  const data = [
+    {
+      name: "Civil",
+      paper_count: departmentAnalytics.Civil,
+    },
+    {
+      name: "Architecture",
+      paper_count: departmentAnalytics.Architecture,
+    },
+
+    {
+      name: "Electronics and Computer",
+      paper_count: departmentAnalytics["Electronics and Computer"],
+    },
+
+    {
+      name: "Mechanical",
+      paper_count: departmentAnalytics.Mechanical,
+    },
+
+    {
+      name: "Electrical",
+      paper_count: departmentAnalytics.Electrical,
+    },
+
+    {
+      name: "Applied Science and Chemical",
+      paper_count: departmentAnalytics["Applied Science and Chemical"],
+    },
+  ];
+
+  console.log("Analytic:", departmentAnalytics);
 
   const department = new Set();
 
@@ -28,8 +113,7 @@ const PaperAnalytics = () => {
   return (
     <div>
       <div className="form-group mt-2">
-        <label>Department</label>
-        <Select
+        {/* <Select
           style={{ width: "300px", marginLeft: "5px" }}
           // onChange={this.onChange}
           name="department"
@@ -37,7 +121,26 @@ const PaperAnalytics = () => {
           {Array.from(department).map((dept) => (
             <MenuItem value={dept}>{dept}</MenuItem>
           ))}
-        </Select>
+        </Select> */}
+
+        <BarChart
+          width={1400}
+          height={500}
+          data={data}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Bar dataKey="paper_count" fill="#8884d8" />
+        </BarChart>
       </div>
     </div>
   );
