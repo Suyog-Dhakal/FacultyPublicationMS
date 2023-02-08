e58e6fa451b924960bc7c032c186fd20a070164c
Suyog-Dhakal <suyogdhakal8@gmail.com>
2023-02-08 17:04:30 +0545

    electronics and computer department account created

--- source/frontend/src/components/papers/Scraping_test.js
+++ source/frontend/src/components/papers/Scraping_test.js
@@ -9,7 +9,7 @@ async function getCitation() {
   let driver = new Builder().forBrowser("chrome").build();
 
   await driver.get(
-    "https://scholar.google.com/citations?user=H74NGRUAAAAJ&hl=en&oi=ao"
+    "https://scholar.google.com/citations?hl=en&user=Wj_P3dIAAAAJ"
   );
 
   await driver.findElement(By.css("button#gsc_bpf_more")).click();
