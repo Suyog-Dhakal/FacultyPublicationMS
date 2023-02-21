ea76f6e6a507e8dded8f36122d9359bc8c7ce859
Ruja Awal <awalruja2016@gmail.com>
2023-02-21 11:10:29 +0545

    Course evaluation form done

--- source/frontend/src/components/CourseEvaluationForm.jsx
+++ source/frontend/src/components/CourseEvaluationForm.jsx
@@ -27,240 +27,280 @@ const CourseEvaluationForm = () => {
     // handle form submission here
     alert("Your answers have been recorded!");
   };
+  const hStyle = { color: "black" };
+  const smallFont = { fontSize: "5px" };
 
   return (
     <form onSubmit={handleSubmit}>
       <FormControl component="fieldset">
-        <FormLabel component="legend">
-          1. How satisfied were you with the course content?
+        <div style={{ display: "inline-block" }}>
+          <FormLabel component="legend" style={hStyle}>
+            1. How satisfied were you with the course content?
+          </FormLabel>
+
+          <RadioGroup
+            style={{ display: "inline-block", marginRight: "10px" }}
+            name="courseContent"
+            value={courseContent}
+            onChange={(e) => setCourseContent(e.target.value)}
+          >
+            <FormControlLabel
+              value="verySatisfied"
+              control={<Radio required />}
+              label="Very satisfied"
+            />
+            <FormControlLabel
+              value="satisfied"
+              control={<Radio required />}
+              label="Satisfied"
+            />
+            <FormControlLabel
+              value="neutral"
+              control={<Radio required />}
+              label="Neutral"
+            />
+            <FormControlLabel
+              value="dissatisfied"
+              control={<Radio required />}
+              label="Dissatisfied"
+            />
+            <FormControlLabel
+              value="veryDissatisfied"
+              control={<Radio required />}
+              label="Very dissatisfied"
+            />
+          </RadioGroup>
+        </div>
+
+        <FormLabel component="legend" style={hStyle}>
+          2. How was the knowledge of the instructor about the subject matter?
         </FormLabel>
         <RadioGroup
-          name="courseContent"
-          value={courseContent}
-          onChange={(e) => setCourseContent(e.target.value)}
+          style={{ display: "inline-block", marginRight: "10px" }}
+          name="knowledge"
+          value={knowledge}
+          onChange={(e) => setKnowledge(e.target.value)}
         >
           <FormControlLabel
             value="verySatisfied"
-            control={<Radio />}
+            control={<Radio required />}
             label="Very satisfied"
           />
           <FormControlLabel
             value="satisfied"
-            control={<Radio />}
+            control={<Radio required />}
             label="Satisfied"
           />
           <FormControlLabel
             value="neutral"
-            control={<Radio />}
+            control={<Radio required />}
             label="Neutral"
           />
           <FormControlLabel
             value="dissatisfied"
-            control={<Radio />}
+            control={<Radio required />}
             label="Dissatisfied"
           />
           <FormControlLabel
             value="veryDissatisfied"
-            control={<Radio />}
+            control={<Radio required />}
             label="Very dissatisfied"
           />
         </RadioGroup>
+
+        <FormLabel component="legend" style={hStyle}>
+          3. How prepared the instructor was for each class?
+        </FormLabel>
+        <RadioGroup
+          style={{ display: "inline-block", marginRight: "10px" }}
+          name="preparedness"
+          value={preparedness}
+          onChange={(e) => setPreparedness(e.target.value)}
+        >
+          <FormControlLabel
+            value="verySatisfied"
+            control={<Radio required />}
+            label="Very satisfied"
+          />
+          <FormControlLabel
+            value="satisfied"
+            control={<Radio required />}
+            label="Satisfied"
+          />
+          <FormControlLabel
+            value="neutral"
+            control={<Radio required />}
+            label="Neutral"
+          />
+          <FormControlLabel
+            value="dissatisfied"
+            control={<Radio required />}
+            label="Dissatisfied"
+          />
+          <FormControlLabel
+            value="veryDissatisfied"
+            control={<Radio required />}
+            label="Very dissatisfied"
+          />
+        </RadioGroup>
+
+        <FormLabel component="legend" style={hStyle}>
+          4. How effective was the communication in the classroom?
+        </FormLabel>
+        <RadioGroup
+          style={{ display: "inline-block", marginRight: "10px" }}
+          name="communication"
+          value={communication}
+          onChange={(e) => setCommunication(e.target.value)}
+        >
+          <FormControlLabel
+            value="verySatisfied"
+            control={<Radio required />}
+            label="Very satisfied"
+          />
+          <FormControlLabel
+            value="satisfied"
+            control={<Radio required />}
+            label="Satisfied"
+          />
+          <FormControlLabel
+            value="neutral"
+            control={<Radio required />}
+            label="Neutral"
+          />
+          <FormControlLabel
+            value="dissatisfied"
+            control={<Radio required />}
+            label="Dissatisfied"
+          />
+          <FormControlLabel
+            value="veryDissatisfied"
+            control={<Radio required />}
+            label="Very dissatisfied"
+          />
+        </RadioGroup>
+
+        <FormLabel component="legend" style={hStyle}>
+          5. How well the instructor responded to the students'questions?
+        </FormLabel>
+        <RadioGroup
+          style={{ display: "inline-block", marginRight: "10px" }}
+          name="respond"
+          value={respond}
+          onChange={(e) => setRespond(e.target.value)}
+        >
+          <FormControlLabel
+            value="verySatisfied"
+            control={<Radio required />}
+            label="Very satisfied"
+          />
+          <FormControlLabel
+            value="satisfied"
+            control={<Radio required />}
+            label="Satisfied"
+          />
+          <FormControlLabel
+            value="neutral"
+            control={<Radio required />}
+            label="Neutral"
+          />
+          <FormControlLabel
+            value="dissatisfied"
+            control={<Radio required />}
+            label="Dissatisfied"
+          />
+          <FormControlLabel
+            value="veryDissatisfied"
+            control={<Radio required />}
+            label="Very dissatisfied"
+          />
+        </RadioGroup>
+
+        <FormLabel component="legend" style={hStyle}>
+          6. How positive was the relation of instructor with the students?
+        </FormLabel>
+        <RadioGroup
+          style={{ display: "inline-block", marginRight: "10px" }}
+          name="relation"
+          value={relation}
+          onChange={(e) => setRelation(e.target.value)}
+        >
+          <FormControlLabel
+            value="verySatisfied"
+            control={<Radio required />}
+            label="Very satisfied"
+          />
+          <FormControlLabel
+            value="satisfied"
+            control={<Radio required />}
+            label="Satisfied"
+          />
+          <FormControlLabel
+            value="neutral"
+            control={<Radio required />}
+            label="Neutral"
+          />
+          <FormControlLabel
+            value="dissatisfied"
+            control={<Radio required />}
+            label="Dissatisfied"
+          />
+          <FormControlLabel
+            value="veryDissatisfied"
+            control={<Radio required />}
+            label="Very dissatisfied"
+          />
+        </RadioGroup>
+
+        <FormLabel component="legend" style={hStyle}>
+          7. How well the instuctor provided the course materials that helped
+          the students?
+        </FormLabel>
+        <RadioGroup
+          style={{ display: "inline-block", marginRight: "10px" }}
+          name="courseMaterial"
+          value={courseMaterial}
+          onChange={(e) => setCourseMaterial(e.target.value)}
+        >
+          <FormControlLabel
+            value="verySatisfied"
+            control={<Radio required />}
+            label="Very satisfied"
+          />
+          <FormControlLabel
+            value="satisfied"
+            control={<Radio required />}
+            label="Satisfied"
+          />
+          <FormControlLabel
+            value="neutral"
+            control={<Radio required />}
+            label="Neutral"
+          />
+          <FormControlLabel
+            value="dissatisfied"
+            control={<Radio required />}
+            label="Dissatisfied"
+          />
+          <FormControlLabel
+            value="veryDissatisfied"
+            control={<Radio required />}
+            label="Very dissatisfied"
+          />
+        </RadioGroup>
+        <Button
+          type="submit"
+          variant="contained"
+          color="primary"
+          style={{
+            fontSize: "0.8rem",
+            padding: "5px 10px",
+            width: "100px",
+          }}
+        >
+          Submit
+        </Button>
       </FormControl>
-
-      <FormLabel component="legend">
-        2. How was the knowledge of the instructor about the subject matter?
-      </FormLabel>
-      <RadioGroup
-        name="knowledge"
-        value={knowledge}
-        onChange={(e) => setKnowledge(e.target.value)}
-      >
-        <FormControlLabel
-          value="verySatisfied"
-          control={<Radio />}
-          label="Very satisfied"
-        />
-        <FormControlLabel
-          value="satisfied"
-          control={<Radio />}
-          label="Satisfied"
-        />
-        <FormControlLabel value="neutral" control={<Radio />} label="Neutral" />
-        <FormControlLabel
-          value="dissatisfied"
-          control={<Radio />}
-          label="Dissatisfied"
-        />
-        <FormControlLabel
-          value="veryDissatisfied"
-          control={<Radio />}
-          label="Very dissatisfied"
-        />
-      </RadioGroup>
-
-      <FormLabel component="legend">
-        3. How prepared the instructor was for each class?
-      </FormLabel>
-      <RadioGroup
-        name="preparedness"
-        value={preparedness}
-        onChange={(e) => setPreparedness(e.target.value)}
-      >
-        <FormControlLabel
-          value="verySatisfied"
-          control={<Radio />}
-          label="Very satisfied"
-        />
-        <FormControlLabel
-          value="satisfied"
-          control={<Radio />}
-          label="Satisfied"
-        />
-        <FormControlLabel value="neutral" control={<Radio />} label="Neutral" />
-        <FormControlLabel
-          value="dissatisfied"
-          control={<Radio />}
-          label="Dissatisfied"
-        />
-        <FormControlLabel
-          value="veryDissatisfied"
-          control={<Radio />}
-          label="Very dissatisfied"
-        />
-      </RadioGroup>
-
-      <FormLabel component="legend">
-        4. How effective was the communication in the classroom?
-      </FormLabel>
-      <RadioGroup
-        name="communication"
-        value={communication}
-        onChange={(e) => setCommunication(e.target.value)}
-      >
-        <FormControlLabel
-          value="verySatisfied"
-          control={<Radio />}
-          label="Very satisfied"
-        />
-        <FormControlLabel
-          value="satisfied"
-          control={<Radio />}
-          label="Satisfied"
-        />
-        <FormControlLabel value="neutral" control={<Radio />} label="Neutral" />
-        <FormControlLabel
-          value="dissatisfied"
-          control={<Radio />}
-          label="Dissatisfied"
-        />
-        <FormControlLabel
-          value="veryDissatisfied"
-          control={<Radio />}
-          label="Very dissatisfied"
-        />
-      </RadioGroup>
-
-      <FormLabel component="legend">
-        5. Ho well the instructor responded to the students'questions?
-      </FormLabel>
-      <RadioGroup
-        name="respond"
-        value={respond}
-        onChange={(e) => setRespond(e.target.value)}
-      >
-        <FormControlLabel
-          value="verySatisfied"
-          control={<Radio />}
-          label="Very satisfied"
-        />
-        <FormControlLabel
-          value="satisfied"
-          control={<Radio />}
-          label="Satisfied"
-        />
-        <FormControlLabel value="neutral" control={<Radio />} label="Neutral" />
-        <FormControlLabel
-          value="dissatisfied"
-          control={<Radio />}
-          label="Dissatisfied"
-        />
-        <FormControlLabel
-          value="veryDissatisfied"
-          control={<Radio />}
-          label="Very dissatisfied"
-        />
-      </RadioGroup>
-
-      <FormLabel component="legend">
-        6. How positive was the relation of instructor with the students?
-      </FormLabel>
-      <RadioGroup
-        name="relation"
-        value={relation}
-        onChange={(e) => setRelation(e.target.value)}
-      >
-        <FormControlLabel
-          value="verySatisfied"
-          control={<Radio />}
-          label="Very satisfied"
-        />
-        <FormControlLabel
-          value="satisfied"
-          control={<Radio />}
-          label="Satisfied"
-        />
-        <FormControlLabel value="neutral" control={<Radio />} label="Neutral" />
-        <FormControlLabel
-          value="dissatisfied"
-          control={<Radio />}
-          label="Dissatisfied"
-        />
-        <FormControlLabel
-          value="veryDissatisfied"
-          control={<Radio />}
-          label="Very dissatisfied"
-        />
-      </RadioGroup>
-
-      <FormLabel component="legend">
-        7. How well the instuctor provided the course materials that helped the
-        students?
-      </FormLabel>
-      <RadioGroup
-        name="courseMaterial"
-        value={courseMaterial}
-        onChange={(e) => setCourseMaterial(e.target.value)}
-      >
-        <FormControlLabel
-          value="verySatisfied"
-          control={<Radio />}
-          label="Very satisfied"
-        />
-        <FormControlLabel
-          value="satisfied"
-          control={<Radio />}
-          label="Satisfied"
-        />
-        <FormControlLabel value="neutral" control={<Radio />} label="Neutral" />
-        <FormControlLabel
-          value="dissatisfied"
-          control={<Radio />}
-          label="Dissatisfied"
-        />
-        <FormControlLabel
-          value="veryDissatisfied"
-          control={<Radio />}
-          label="Very dissatisfied"
-        />
-      </RadioGroup>
-      <Button
-        type="submit"
-        variant="contained"
-        color="primary"
-        onClick={handleSubmit}
-      >
-        Submit
-      </Button>
     </form>
   );
 };
